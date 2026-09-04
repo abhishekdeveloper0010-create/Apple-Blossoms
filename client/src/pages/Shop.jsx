779e7c039f957fdcd9801b376b670f6b515f7307
abhishek.developer0010@gmail.com <abhishekkumar97831@gmail.com>
2026-09-04 09:24:32 +0530

    update shop file code

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -304,7 +304,7 @@ function Shop() {
         setTotalPages(1);
 
         setError(
-          "Products load nahi ho pa rahe hain."
+          "Products are not loading."
         );
       } finally {
         if (mounted) {
